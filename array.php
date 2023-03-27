b4a3cad7a3987b21fdac3ccc945d875a77e44330
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-27 10:55:20 +0900

    array_rand

--- array.php
+++ array.php
@@ -1,8 +1,11 @@
 <?php
-    $lunchMen =  array("초밥", "유부초밥", "김밥", "냉면", "리면", "돈까스", "쫄면");
-    for ($i=0; $i < 7; $i++) { 
-        echo $lunchMen[$i];
-        echo " ";
-    }
+    $lunchMen =  array("초밥", "유부초밥", "김밥", "냉면", "라면", "돈까스", "쫄면");
+    $randMenu = $lunchMen[array_rand($lunchMen)];
+    echo $randMenu;
+    
+    // for ($i=0; $i < 7; $i++) { 
+    //     echo $lunchMen[$i];
+    //     echo " ";
+    // }
     
 ?>
