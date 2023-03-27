5515649286a3860d1d7566bfd344983662a95bf0
snoh6839 <116703016+snoh6839@users.noreply.github.com>
2023-03-27 12:09:50 +0900

    multi dementional array

--- array.php
+++ array.php
@@ -8,12 +8,12 @@
     //     echo " ";
     // }
     
-    $month = range(1, 12);
-    $week = array("일", "월", "화", "수", "목", "금", "토");
-    $day = range(1, 31);
-    echo $month[array_rand($month)],"월", " ";
-    echo $day[array_rand($day)],"일", " ";
-    echo $week[array_rand($week)],"요일", "\n";
+    // $month = range(1, 12);
+    // $week = array("일", "월", "화", "수", "목", "금", "토");
+    // $day = range(1, 31);
+    // echo $month[array_rand($month)],"월", " ";
+    // echo $day[array_rand($day)],"일", " ";
+    // echo $week[array_rand($week)],"요일", "\n";
     
     // 두 날짜 사이의 임의의 날짜를 생성
     function randomDate($start_date, $end_date){
@@ -28,11 +28,24 @@
         return date('Y-m-d', $val);
     }
 
-    $start_date = '2023-03-01';
-    $end_date = '2023-08-01';
+    $start_date = '2023-04-01';
+    $end_date = '2023-04-30';
 
-    echo randomDate($start_date, $end_date), "\n";
+    echo "4월달 초밥 먹는 날: ", randomDate($start_date, $end_date), "\n";
 
-    $arrayName = array("key1" => "val1", "key2" => "var2" );
-    echo $arrayName["key1"];
+    // $arrayName = array("key1" => "val1", "key2" => "var2" );
+    // echo $arrayName["key1"];
+
+    $recipe = array("계란초밥" => "계란", "새우초밥" =>"새우", "장어초밥" =>"장어", "유부초밥" =>"유부", "연어초밥" =>"연어", "광어초밥" =>"광어", "참치초밥" =>"참치", "날치알초밥" =>"날치알", "문어초밥" =>"문어", "가리비초밥" =>"가리비", "북방조개초밥" => "북방조개");
+    $randomSushi = array_rand($recipe);
+    $ingredient = $recipe[$randomSushi];
+    echo "메뉴: ",$randomSushi, ", 주재료: ", $ingredient, "\n";
+
+    $multiDimention = array(
+    range(1, 20),
+    array("못 먹어", "맘껏 먹어")
+    );
+    echo "먹을 갯수: ", $multiDimention[0][9];
+
+    //8번 출력 echo $arr_temp[1][3];
 ?>
